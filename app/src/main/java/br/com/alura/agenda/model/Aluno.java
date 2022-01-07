2230f95aeef165713d1972a2893ed2dec82be438
danielh-oliveira <danielh_oliveira@yahoo.com.br>
2022-01-07 11:59:50 -0300

    Criei a logica que pega o texto, instancia o model Aluno e joga na tela as infos via Toas. Falta salvar o aluno na agenda.

---
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -0,0 +1,26 @@
+package br.com.alura.agenda.model;
+
+public class Aluno {
+    private final String nome;
+    private final String telefone;
+    private final String email;
+
+    public Aluno(String nome, String telefone, String email) {
+
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
--- app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
@@ -3,8 +3,13 @@ package br.com.alura.agenda.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.com.alura.agenda.R;
+import br.com.alura.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -12,5 +17,30 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+//        É importante fazer a busca das views fora do listener
+//        para que nao fique buscando as views em toda a acao.
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        Button botaoSalvarFormulario = findViewById(R.id.activity_formulario_aluno_botao_enviar);
+        botaoSalvarFormulario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno alunoCriado = new Aluno(nome, telefone, email);
+                Toast.makeText(
+                        FormularioAlunoActivity.this,
+                        alunoCriado.getNome() + " - "
+                                + alunoCriado.getTelefone() + " - "
+                                + alunoCriado.getEmail(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
